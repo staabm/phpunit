86024f0a028b7037872df425723fe8dd644c0262
Markus Staab <maggus.staab@googlemail.com>
2024-09-26 10:55:13 +0200

    Update job.php

--- job.php
+++ job.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 $_SERVER['argv'][] = '--do-not-cache-result';
 $_SERVER['argv'][] = '--no-configuration';
-$_SERVER['argv'][] = 'C:\dvl\Workspace\phpunit\tests\end-to-end\regression' . '/2137/Issue2137Test.php';
+$_SERVER['argv'][] = __DIR__.'/tests/end-to-end/regression/2137/Issue2137Test.php';
 $_SERVER['argv'][] = '--filter';
 $_SERVER['argv'][] = 'BrandService';
 
-require 'C:\dvl\Workspace\phpunit\vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 (new PHPUnit\TextUI\Application)->run($_SERVER['argv']);
