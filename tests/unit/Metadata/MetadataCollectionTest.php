bb21cb7e4ea895e4396d8f3a4ecd57a72390b180
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-08-01 10:08:42 +0200

    Reorder

--- tests/unit/Metadata/MetadataCollectionTest.php
+++ tests/unit/Metadata/MetadataCollectionTest.php
@@ -571,14 +571,6 @@ final class MetadataCollectionTest extends TestCase
         $this->assertTrue($collection->asArray()[0]->isWithoutErrorHandler());
     }
 
-    public function test_Can_be_filtered_for_IgnorePhpunitWarnings(): void
-    {
-        $collection = $this->collectionWithOneOfEach()->isIgnorePhpunitWarnings();
-
-        $this->assertCount(1, $collection);
-        $this->assertTrue($collection->asArray()[0]->isIgnorePhpunitWarnings());
-    }
-
     private function collectionWithOneOfEach(): MetadataCollection
     {
         return MetadataCollection::fromArray(
@@ -647,7 +639,6 @@ final class MetadataCollectionTest extends TestCase
                 Metadata::usesMethod('', ''),
                 Metadata::withEnvironmentVariableOnClass('foo', 'bar'),
                 Metadata::withoutErrorHandler(),
-                Metadata::ignorePhpunitWarnings(null),
             ],
         );
     }
